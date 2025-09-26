3c80eb50c6e66398b6bcc49b5729128eb42ac696
Manish Kumar <ios.curator@gmail.com>
2025-09-26 17:47:46 +0530

    disable login with meta & Email

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -31,18 +31,18 @@ export default function LoginModal({ isOpen, onClose, message }: Props) {
           >
             Continue with Google
           </button>
-          <button
+          {/* <button
             onClick={() => signIn("facebook", { callbackUrl: "/" })}
             className="w-full px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           >
             Continue with Meta
-          </button>
-          <button
+          </button> */}
+          {/* <button
             onClick={() => signIn("email", { callbackUrl: "/" })}
             className="w-full px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-900"
           >
             Continue with Email
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
