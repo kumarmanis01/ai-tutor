874a206acb72c759c8be2fba990654d5a23e907d
Manish Kumar <ios.curator@gmail.com>
2025-09-29 17:48:59 +0530

    tested in local modified image domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    // Add the allowed external image domain here
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 export default nextConfig;
