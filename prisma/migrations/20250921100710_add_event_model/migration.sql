85aca78d688f3477fbc2b793792817857d964fec
Manish Kumar <ios.curator@gmail.com>
2025-10-01 13:13:29 +0530

    migrate to postgres

--- prisma/migrations/20250921100710_add_event_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Event" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT,
-    "type" TEXT NOT NULL,
-    "metadata" JSONB,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- prisma/migrations/20250929071347_add_chat_history/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ChatHistory" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    "messages" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "ChatHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
