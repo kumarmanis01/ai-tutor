bb70414083b3e205985438bb37aa3885b5939ef6
Manish Kumar <ios.curator@gmail.com>
2025-11-27 21:02:31 +0530

    feat(migration): remove lastFreeQuestionsUpdate column and related migration files

--- prisma/migrations/20251127_drop_lastFreeQuestionsUpdate/migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration: drop lastFreeQuestionsUpdate column from User
--- Created by automation: drop nullable timestamp column
-
-ALTER TABLE "User"
-DROP COLUMN IF EXISTS "lastFreeQuestionsUpdate";
