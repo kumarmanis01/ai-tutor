4681e42988ad93011b24821e614171df91a00187
Manish Kumar <ios.curator@gmail.com>
2025-12-18 12:10:26 +0530

    chore(migrations): add init migration

--- prisma/migrations/20251216115117_add_lifecycle_to_class_subject/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ClassLevel" ADD COLUMN     "lifecycle" "SoftDeleteStatus" NOT NULL DEFAULT 'active';
-
--- AlterTable
-ALTER TABLE "SubjectDef" ADD COLUMN     "lifecycle" "SoftDeleteStatus" NOT NULL DEFAULT 'active';
