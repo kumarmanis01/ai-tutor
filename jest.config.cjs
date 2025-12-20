049fb9d0f523b97ed947128ccca1d165afd0e2ba
Manish Kumar <ios.curator@gmail.com>
2025-12-20 07:19:06 +0530

    test(alerts): remove duplicate flaky tests; ignore integration tests in jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,10 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
-};
 /** Jest config using ts-jest for TypeScript tests */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // run only unit tests by default; integration tests are excluded
   testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/tests/integration/'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   globals: {
     'ts-jest': {
