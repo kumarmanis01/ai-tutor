12739dfb0c653cdd24b488e3560ad8ce568c62d1
Manish Kumar <ios.curator@gmail.com>
2026-04-17 07:05:18 +0530

    Parent dashboard: use server-provided mastery tooltip; add opt-in benchmarking and test; fix TS in examReadiness

---
+++ jest.temp.unit.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'],
+  testPathIgnorePatterns: ['/tests/integration/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/lib/(.*)\\.js$': '<rootDir>/lib/$1.ts',
+    '^@/lib/(.*)\\.ts$': '<rootDir>/lib/$1.ts',
+    '^@/lib/(.*)$': ['<rootDir>/lib/$1.ts', '<rootDir>/lib/$1/index.ts', '<rootDir>/lib/$1'],
+    '^@/(.*)\\.js$': '<rootDir>/src/$1.ts',
+    '^@/(.*)$': ['<rootDir>/src/$1', '<rootDir>/$1']
+  },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  setupFiles: ['<rootDir>/tests/setup/forceTestNodeEnv.cjs'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup/jsdomPolyfills.ts', '<rootDir>/tests/setup/normalizePaths.cjs', '<rootDir>/tests/setup/normalizePaths.ts', '<rootDir>/tests/setup/prismaEnsureColumns.ts', '<rootDir>/tests/setup/loggerTeardown.ts'],
+  forceExit: true,
+  collectCoverage: false,
+};
