d30567310e96f06d2cc6f4ee818c6887c89d9be4
Manish Kumar (kumarmanis01) <ios.curator@gmail.com>
2026-05-01 20:26:46 +0530

    remove the top padding for student area from layout

--- app/(student)/layout.tsx
+++ app/(student)/layout.tsx
@@ -233,7 +233,7 @@ export default async function StudentLayout({ children }: { children: React.Reac
               showProfileGate={showProfileGate}
               initialProfileData={initialProfileData}
             >
-              <div id="student-main" className="pt-[44px] pb-16 md:pb-0">
+              <div id="student-main" className="pt-[1px] pb-16 md:pb-0">
                 {children}
               </div>
             </StudentLayoutShell>
